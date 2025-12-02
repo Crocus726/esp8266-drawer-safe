33a3031ca3191bde4e1ce5b560d6f265ccdc3840
Crocus726 <crocus726@gmail.com>
2025-12-03 01:02:44 +0900

    Improve keypad input logic and pin configuration
    Refactor keypad input handling and update pin definitions.

--- esp8266-drawer-safe/esp8266-drawer-safe.ino
+++ esp8266-drawer-safe/esp8266-drawer-safe.ino
@@ -1,7 +1,3 @@
-/**
-
-*/
-
 /* RFID-RC522 모듈 사용 라이브러리 */
 #include <MFRC522Constants.h>
 #include <MFRC522Debug.h>
@@ -22,19 +18,24 @@ MFRC522DriverSPI driver{ss_pin}; // Create SPI driver
 //MFRC522DriverI2C driver{};     // Create I2C driver
 MFRC522 mfrc522{driver};         // Create MFRC522 instance
 
-String rfid_pass_key = "767bc6db";
-
 /* 터치 키패드 관련 설정 */
-#define scl_pin 8;
-#define sdo_pin 9;
-byte keypad_input;
+#define scl_pin 5
+#define sdo_pin 4
+
+/* 전역 변수 설정 */
+byte current_key;
+byte last_key;
+
+/* 인증 키(키패드 비밀번호, RFID UID) 설정 */
+String rfid_pass_key = "767bc6db";
+String keypad_pass_key = "14116";
 
 void setup() {
   Serial.begin(115200); while (!Serial); // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4).
   
   mfrc522.PCD_Init();    // Init MFRC522 board.
   MFRC522Debug::PCD_DumpVersionToSerial(mfrc522, Serial);	// Show details of PCD - MFRC522 Card Reader details.
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+  Serial.println("Scan PICC to see UID, SAK, type, and data blocks...");
 
   pinMode(scl_pin, OUTPUT); pinMode(sdo_pin, INPUT);
 
@@ -42,10 +43,26 @@ void setup() {
 }
 
 void loop() {
-  keypad_input = readKeypad();
-  if (keypad_input) {
+  if (readKeypad() == 13) {
     Serial.println("키패드 입력이 인식되었습니다.");
-    // 파란색 LED 끄고
+    // LED 파란색 끄고 주황색 켜기
+    String password_input;
+    for (digit = 1; digit <= 4; digit ++) {
+      while (readKeypad() != 0) {} // 키패드에서 손을 뗄 때까지 대기
+      while (readKeypad() == 0) {} // 다시 누를 때까지 대기
+      current_key = readKeypad();
+      Serial.print(digit); Serial.print("번째 자리: "); Serial.println(current_key);
+      password_input += String(current_key);
+    }
+    Serial.print("입력된 비밀번호: "); Serial.println(password_input);
+
+    /* 패스워드가 등록된 패스워드인지 확인하고 문 열기 또는 무시하기 */
+    if (password_input == keypad_pass_key) {
+      Serial.println("인식된 패스워드는 등록되어 있는 패스워드입니다.");
+      //doorOpen();
+    } else {
+      Serial.println("인식된 패스워드는 등록되어 있지 않은 패스워드입니다.");
+    }
   }
 
   if (mfrc522.PICC_IsNewCardPresent()) {
@@ -71,12 +88,10 @@ void loop() {
       Serial.println("인식된 RFID 카드는 등록되어 있지 않은 카드입니다.");
     }
   }
-  //delay(500);
 }
 
-/* 키패드 입력을 인식하고, 입력된 키를 Byte 형태로 반환 */
-/*
-Byte readKeypad(void) {
+/* 키패드 입력을 인식하고, 입력된 키를 byte 형태로 반환 */
+byte readKeypad(void) {
   byte key_count;
   byte key_state = 0;
   for (key_count = 1; key_count <= 16; key_count ++) {
@@ -86,7 +101,6 @@ Byte readKeypad(void) {
   }
   return key_state;
 }
-*/
 
 /* 키패드 비밀번호 또는 RFID UID 인증에 성공하면 실행 */
 void doorOpen() {
